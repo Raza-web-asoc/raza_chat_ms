5215a0e497cef381c60f22b49aafeaff6c6c5c6e
JuanCarlosPrieto <juan.carlos@caseware.com>
2025-01-27 21:37:07 -0500

    this is websockets for back

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.middleware.cors import CORSMiddleware
 from app.routers.chat import router_chats
 
 app = FastAPI()
+
+connections = []
 
 app.add_middleware(
     CORSMiddleware,
@@ -11,6 +13,22 @@ app.add_middleware(
     allow_methods=[""],
     allow_headers=[""],
 )
+
+@app.websocket("/ws")
+async def  websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    connections.append(websocket)
+
+    try:
+        while True:
+            data = await websocket.receive_text()
+            for connection in connections:
+                await connection.send_text(data)
+    
+    except WebSocketDisconnect:
+        connections.remove(websocket)
+
+        
 
 # Registrar el router
 app.include_router(router_chats, prefix="/chats", tags=["Chats"])
